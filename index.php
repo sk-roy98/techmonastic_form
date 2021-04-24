7e94a166cc26b77b1eae0930e39da4e4f93e3472
Souvik Roy <roysouvik98@gmail.com>
2021-04-24 13:17:27 +0530

    validation

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+<?php
+if (isset($_POST["register"])){
+
+    if(empty($_POST['email'])||
+    empty($_POST['password'])||
+    empty($_POST['confirm'])){
+        echo('please fill the required fields!'); 
+    }
+}
+    else if($_POST['password'] !== $_POST['confirm']){
+        echo('password and confirm password should match!');
+    }
+
+    else{
+    //   // uploading
+    //   $msg = "";
+    //   // If upload button is clicked ...
+    // if (isset($_POST['register'])){
+    //     $email = mysqli_real_escape_string($conn, $_REQUEST['email']);
+    //     $password = mysqli_real_escape_string($conn, $_REQUEST['password']);
+    //     $identity = mysqli_real_escape_string($conn, $_REQUEST['identity']);
+    //     $city = mysqli_real_escape_string($conn, $_REQUEST['city']);
+    //     $gender = mysqli_real_escape_string($conn, $_REQUEST['gender']);
+    //     $filename = $_FILES["image"]["name"];
+    //     $tempname = $_FILES["image"]["tmp_name"];    
+    //         $folder = "user/".$filename;
+              
+    //         include('config/db_connect.php');
+      
+    //         // Get all the submitted data from the form
+    //         $sql = "INSERT INTO `user`(`id`, `email`, `password`, `identity`, `city`, `gender`, `image`) VALUES 
+    //         ('$email','$password','$identity','$city','$gender','$filename')";
+            
+            
+    //         // Execute query
+    //         mysqli_query($db, $sql);
+              
+    //         // Now let's move the uploaded image into the folder: user
+    //         if (move_uploaded_file($tempname, $folder))  {
+    //             $msg = "Image uploaded successfully";
+    //         }else{
+    //             $msg = "Failed to upload image";
+    //       }
+    // }
+}
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,41 +57,42 @@
 </head>
 <body>
     <div class="formValidation">
-    <form action="register.php" method="POST" enctype="multipart/form-data" >
+    <form action=" " method="post" enctype="multipart/form-data" >
         <label for="mail">Email</label><br>
-        <input type="email" id="mail" name="email"><br>
+        <input  class="input box" type="email" id="mail" name="email"><br>
 
         <label for="pw">Password</label><br>
-        <input type="password" id="pw" name ="password"><br>
+        <input class="input box" type="password" id="pw" name ="password"><br>
 
-        <label for="pw">Confirm Password</label><br>
-        <input type="Password" id="confirm" name ="confirm"><br>
+        <label for="confirm">Confirm Password</label><br>
+        <input class="input box" type="Password" id="confirm" name ="confirm"><br>
 
-        <label for="identity1, identity2", name= 'identity'>choose identity</label><br>
-        <input type="checkbox" id="Identity1" name="identity[]" value="Aadhar">
-        <label for="Identity1"> Aadhar card</label><br>
-        <input type="checkbox" id="Identity2" name="identity[]" value="Pan">
-        <label for="identity2"> Pan card</label><br>
+        <label for="identity1">choose identity</label><br>
+        <input class="input" type="checkbox" id="Identity" name="identity[]" value="Aadhar">
+        <label for="Identity"> Aadhar card</label><br>
+        <input class="input" type="checkbox" id="Identity" name="identity[]" value="Pan">
+        <label for="Identity"> Pan card</label><br>
+
 
         <label for="city">Choose a city:</label>
-        <select id="city" name="city">
+        <select class="input" id="city" name="city">
         <option value="siliguri">Siliguri</option>
         <option value="darjeeling">Darjeeling</option>
         <option value="kalimpong">Kalimpong</option>
         </select><br>
-
-        <input type="radio" id="male" name="gender" value="male">
+        <label>Choose gender</label><br>
+        <input class="input" type="radio" id="male" name="gender" value="male">
         <label for="male">Male</label><br>
-        <input type="radio" id="female" name="gender" value="female">
+        <input class="input" type="radio" id="female" name="gender" value="female">
         <label for="female">Female</label><br>
-        <input type="radio" id="other" name="gender" value="other">
+        <input class="input" type="radio" id="other" name="gender" value="other">
         <label for="other">Other</label><br>
 
-        <input type="file" id="file" name="image" accept="image/png , image/jpeg" onchange="uploaded(event)" name="image" style="display: none;">
+        <input class="input" type="file" id="file" name="image" accept="image/png , image/jpeg" onchange="uploaded(event)" name="image" style="display: none;">
         <label for="file" style="cursor: pointer;">Upload image here</label><br>
         <img id="display" width="200"/>
 
-        <button type="submit" name = "register" class="submit"> register </button>
+        <input class="button" type="submit" name = "register" value="Register"/>
     </form>
     </div>
     <script>
